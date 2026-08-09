ef3c8265394de70a4c0e2da94472bff1f1bff31d
jvaswb <bpsixta@gmail.com>
2026-08-09 14:19:46 +0000

    Revert "aee: Add dummy aee_modem_warning()"
    This reverts commit b2c915d767bab962acc7a82e340e4862a74929ae.

--- libaedv/aee.c
+++ libaedv/aee.c
@@ -22,11 +22,6 @@ int aee_system_warning(const char *lib, const char *path, unsigned int options, 
     return 0;
 }
 
-int aee_modem_warning(const char *lib,const char* path,unsigned int options, const char *log_msg,  const char *ver) {
-    ALOGD("[%s]: lib: %s, path: %s, options: %u, log_msg: %s, ver: %s", __func__, lib, path, options, log_msg, ver);
-    return 0;
-}
-
 int aee_log_msg(const char *lib, const char *path, unsigned int options, const char *log_msg, const char *ver) {
     ALOGD("[%s]: lib: %s, path: %s, options: %u, log_msg: %s, ver: %s", __func__, lib, path, options, log_msg, ver);
     return 0;
